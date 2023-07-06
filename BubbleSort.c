cfa2af39ccb90012fc6265f9314c76d2b98ec859
WinterirlK <138369381+WinterirlK@users.noreply.github.com>
2023-07-06 19:33:24 +0200

    Create BubbleSort.c

---
+++ BubbleSort.c
@@ -0,0 +1,13 @@
+void bubbleSort(int arr[]) {
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int i, j;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
